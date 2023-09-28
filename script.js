2640f8ef6e9beeacf1ab23d56ac756a1c197b693
WhiteLicorice <96515086+WhiteLicorice@users.noreply.github.com>
2023-09-28 11:24:44 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Fake News Detector
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  A userscript that connects with a Node-hosted machine learning model to determine if an article or a post is authentic or otherwise.
+// @author       Rene Andre Jocsing, Kobe Austin Lupac, Chancy Ponce de Leon
+// @match        https://twitter.com/*
+// @match        https://www.facebook.com/*
+// @icon         https://cdn0.iconfinder.com/data/icons/modern-fake-news/500/asp1430a_9_newspaper_fake_news_icon_outline_vector_thin-1024.png
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("The script is live!")
+
+    //TODO: Ping a backend server hosted on some port and spoof results.
+})();
