c6071db9633bfbcf7e9932357e1783e7b10b250b
RB <rbjocsing@up.edu.ph>
2023-09-29 17:30:47 +0800

    Added scaffolding

--- userscript/script.js
+++ userscript/script.js
@@ -10,15 +10,43 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+
+
+
+(function main() {
     'use strict';
 
     console.log("The script is live!")
 
-    //  TODO: Ping a backend server hosted on some port and spoof results.
-    //  TODO: Add buttons to the post div on FB and Twitter
-    //  TODO: Add function for scraping text off an article
-    //  TODO: Add function for sending scraped text to backend server
-    //  TODO: Add function for displaying result of query to the user
+    //    TODO: Ping a backend server hosted on some port and spoof results.
+
+    function isTweet(element) {
+        //  TODO: Return True if the element attribute data-testid = "tweet" else False
+    }
     
+    function addButtonTweeter(element) {
+        //  TODO: Add a "Validate" button to all nodes that are tweets
+        //  HINT: Use GM_addElement(parent_node, tag_name, attributes) method, where parent_node is the node to attach the button, tag_name is the HTML tag of the node to be attached, and attributes is a dictionary of additional attributes
+        //  EXAMPLE:
+        /* 
+        GM_addElement('script', {
+            textContent: 'window.foo = "bar";'
+        });
+
+        GM_addElement('script', {
+            src: 'https://example.com/script.js',
+            type: 'text/javascript'
+        });
+
+        GM_addElement(document.getElementsByTagName('div')[0], 'img', {
+            src: 'https://example.com/image.png'
+        });
+
+        GM_addElement(shadowDOM, 'style', {
+            textContent: 'div { color: black; };'
+        }); 
+        */
+    }
+
+
 })();
