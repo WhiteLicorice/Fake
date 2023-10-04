c1919d1b6fe6098322223b3090566d442a923eda
RB <rbjocsing@up.edu.ph>
2023-10-04 09:48:57 +0800

    Added scraping and button capabilities

--- userscript/script.js
+++ userscript/script.js
@@ -4,49 +4,66 @@
 // @version      0.1
 // @description  A userscript that connects with a Node-hosted machine learning model to determine if an article or a post is authentic or otherwise.
 // @author       Rene Andre Jocsing, Kobe Austin Lupac, Chancy Ponce de Leon
-// @match        https://twitter.com/*
-// @match        https://www.facebook.com/*
 // @icon         https://cdn0.iconfinder.com/data/icons/modern-fake-news/500/asp1430a_9_newspaper_fake_news_icon_outline_vector_thin-1024.png
 // @grant        none
+// @match https://www.philstar.com/*
 // ==/UserScript==
 
-
-
-
-(function main() {
+(function() {
     'use strict';
 
     console.log("The script is live!")
 
-    //    TODO: Ping a backend server hosted on some port and spoof results.
+    var really_fake = false//   Scaffold for machine learning model return value
 
-    function isTweet(element) {
-        //  TODO: Return True if the element attribute data-testid = "tweet" else False
+    function is_fake_news(news_article) {
+        //SCAFFOLD
+        //TODO: Integrate machine learning model in the backend
+
+        return really_fake
+
     }
+
+    function scrape_paragraphs(){
+        var paragraphs = document.querySelectorAll("p")//  Returns an array of all the paragraph elements on the page
+        var news_article = []//  Initialize an array to contain all the paragraph.textContents
+        try {
+            paragraphs.forEach(paragraph => {
+                const paragraph_text = paragraph.textContent.trim()
+                if (paragraph_text.length > 0) {
+                    news_article.push(paragraph_text)
+                }
+            })
+        } catch(error) {
+            console.log('Error parsing paragraphs occured: ${error}')
+        } finally {
+            console.log(news_article.join("\n"))
+            var fake_news = is_fake_news(news_article.join("\n"))
+            if (fake_news) {
+                alert("This is FAKE!!!")
+            }
+            else {
+                alert("This is REAL!!!")
+            }
+        }
+    }
+
+    function add_floating_button(){
+        // Create a floating button element
+        const floatingButton = document.createElement("button")
+        floatingButton.textContent = "DETECT FAKE NEWS"
+        floatingButton.style.position = "fixed"
+        floatingButton.style.bottom = "20px"
+        floatingButton.style.right = "20px"
+        floatingButton.style.zIndex = "9999"
     
-    function addButtonTweeter(element) {
-        //  TODO: Add a "Validate" button to all nodes that are tweets
-        //  HINT: Use GM_addElement(parent_node, tag_name, attributes) method, where parent_node is the node to attach the button, tag_name is the HTML tag of the node to be attached, and attributes is a dictionary of additional attributes
-        //  EXAMPLE:
-        /* 
-        GM_addElement('script', {
-            textContent: 'window.foo = "bar";'
-        });
-
-        GM_addElement('script', {
-            src: 'https://example.com/script.js',
-            type: 'text/javascript'
-        });
-
-        GM_addElement(document.getElementsByTagName('div')[0], 'img', {
-            src: 'https://example.com/image.png'
-        });
-
-        GM_addElement(shadowDOM, 'style', {
-            textContent: 'div { color: black; };'
-        }); 
-        */
+        // Attach a click event listener to the button
+        floatingButton.addEventListener("click", scrape_paragraphs);
+    
+        // Append the button to the body of the page
+        document.body.appendChild(floatingButton);
     }
 
+    add_floating_button()
 
 })();
