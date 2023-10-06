81bb92e747326bc4c2435e0eb491421f64aa607f
RB <rbjocsing@up.edu.ph>
2023-10-06 12:02:53 +0800

    Initial implementation of end-to-end system

---
+++ server/app.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+
+import random
+
+# Declaring our FastAPI instance
+app = FastAPI()
+version = "0.0.0.0.0.0.0.1"
+
+# Replace this list with the actual origins you want to allow
+origins = [
+    "https://www.philstar.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class News(BaseModel):
+	news_body: str
+
+@app.get('/')
+def health_check():
+	return {'health': f'Running version {version} of Fake_API'}
+
+@app.post("/check-news")
+def check_news(news: News):
+    is_fake_news = str(bool(random.randint(0, 1)))   ##  Scaffold: Insert pipe(ML_model) here
+    print(news.news_body)
+    print(is_fake_news)
+    return {"status": is_fake_news}
--- userscript/script.js
+++ userscript/script.js
@@ -7,63 +7,88 @@
 // @icon         https://cdn0.iconfinder.com/data/icons/modern-fake-news/500/asp1430a_9_newspaper_fake_news_icon_outline_vector_thin-1024.png
 // @grant        none
 // @match https://www.philstar.com/*
+// @match https://news.abs-cbn.com/*
+// @match https://www.gmanetwork.com/*
 // ==/UserScript==
 
 (function() {
-    'use strict';
+	'use strict';
 
-    console.log("The script is live!")
+	console.log("The script is live!")
 
-    var really_fake = false//   Scaffold for machine learning model return value
+	var API_ENDPOINT = "http://127.0.0.1:5000/check-news"
 
-    function is_fake_news(news_article) {
-        //SCAFFOLD
-        //TODO: Integrate machine learning model in the backend
+	async function run_script_pipeline(){
+		var processed_article = await scrape_paragraphs()
+		var fake_api_result = await is_fake_news(processed_article)
+		display_is_fake_news(fake_api_result)
+	}
 
-        return really_fake
+	async function display_is_fake_news(api_result){
+		if (api_result) {
+			alert("This is FAKE!!!")
+		}
+		else {
+			alert("This is REAL!!!")
+		}
+	}
 
-    }
+	async function is_fake_news(news_article) {
+		var FAKE_API_CALL = await fetch(API_ENDPOINT, {
+			method: 'POST',
+			body: JSON.stringify({ news_body: news_article }), // Assuming news_article is a string
+			headers: {
+				'Content-Type': 'application/json', // Set the content type to JSON
+			},
+		});
 
-    function scrape_paragraphs(){
-        var paragraphs = document.querySelectorAll("p")//  Returns an array of all the paragraph elements on the page
-        var news_article = []//  Initialize an array to contain all the paragraph.textContents
-        try {
-            paragraphs.forEach(paragraph => {
-                const paragraph_text = paragraph.textContent.trim()
-                if (paragraph_text.length > 0) {
-                    news_article.push(paragraph_text)
-                }
-            })
-        } catch(error) {
-            console.log('Error parsing paragraphs occured: ${error}')
-        } finally {
-            console.log(news_article.join("\n"))
-            var fake_news = is_fake_news(news_article.join("\n"))
-            if (fake_news) {
-                alert("This is FAKE!!!")
-            }
-            else {
-                alert("This is REAL!!!")
-            }
-        }
-    }
+		if (!FAKE_API_CALL.ok) {
+			throw new Error('Network response was not ok');
+		}
 
-    function add_floating_button(){
-        // Create a floating button element
-        const floatingButton = document.createElement("button")
-        floatingButton.textContent = "DETECT FAKE NEWS"
-        floatingButton.style.position = "fixed"
-        floatingButton.style.bottom = "20px"
-        floatingButton.style.right = "20px"
-        floatingButton.style.zIndex = "9999"
-    
-        // Attach a click event listener to the button
-        floatingButton.addEventListener("click", scrape_paragraphs);
-    
-        // Append the button to the body of the page
-        document.body.appendChild(floatingButton);
-    }
+		try {
+			const responseData = await FAKE_API_CALL.json(); // Parse the response as JSON
+			console.log('API Response:', responseData);
+			// Now you can access the data in the responseData variable
+			return ((responseData.status).toLowerCase)
+		} catch (error) {
+			console.error('API Request Error:', error);
+		}
+	}
 
-    add_floating_button()
+	async function scrape_paragraphs(){
+		var paragraphs = document.querySelectorAll("p")//  Returns an array of all the paragraph elements on the page
+		var news_article = []//  Initialize an array to contain all the paragraph.textContents
+		try {
+			paragraphs.forEach(paragraph => {
+				const paragraph_text = paragraph.textContent.trim()
+				if (paragraph_text.length > 0) {
+					news_article.push(paragraph_text)
+				}
+			})
+			console.log(news_article.join("\n"))
+			return (news_article.join("\n"))
+		} catch(error) {
+			console.log('Error parsing paragraphs occured: ${error}')
+		}
+	}
+
+	function add_floating_button(){
+		// Create a floating button element
+		const floatingButton = document.createElement("button")
+		floatingButton.textContent = "DETECT FAKE NEWS"
+		floatingButton.style.position = "fixed"
+		floatingButton.style.bottom = "20px"
+		floatingButton.style.right = "20px"
+		floatingButton.style.zIndex = "9999"
+
+		// Attach a click event listener to the button
+		floatingButton.addEventListener("click", run_script_pipeline);
+
+		// Append the button to the body of the page
+		document.body.appendChild(floatingButton);
+	}
+
+	add_floating_button()
 
 })();
