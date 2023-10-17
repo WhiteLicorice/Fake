82094db6c0213fbafaa5b8515d90ae97f3f2599b
RB <rbjocsing@up.edu.ph>
2023-10-17 22:15:07 +0800

    Prototyping with machine learning model

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-
+import pickle
 from pydantic import BaseModel
 
 import random
@@ -25,14 +25,20 @@ app.add_middleware(
 
 class News(BaseModel):
 	news_body: str
-
+	
+@app.on_event("startup")
+def load_model():
+	global model
+	model = pickle.load(open("model.pkl", "rb"))
+	
 @app.get('/')
 def health_check():
 	return {'health': f'Running version {version} of Fake_API'}
 
 @app.post("/check-news")
 def check_news(news: News):
-    is_fake_news = str(bool(random.randint(0, 1)))   ##  Scaffold: Insert pipe(ML_model) here
+    #is_fake_news = str(bool(random.randint(0, 1)))   ##  Scaffold: Insert pipe(ML_model) here
+    is_fake_news = model.predict(news.news_body)
     print(news.news_body)
     print(is_fake_news)
     return {"status": is_fake_news}
