c46ed1096929f367dc20159ae09dbfb4b30f2b3f
RB <rbjocsing@up.edu.ph>
2023-11-13 11:24:02 +0800

    Changed endpoint to cyclic.sh

--- server/app.py
+++ server/app.py
@@ -16,8 +16,8 @@ import httpx
 app = FastAPI()
 version = "0.0.0.0.0.0.0.1"
 model_id = "svm" 
-model_api = "http://127.0.0.1:6996/predict"             #   Localhost endpoint
-#model_api = "https://fake-ph-ml.cyclic.app/predict"      #   Cyclic enpoint
+#model_api = "http://127.0.0.1:6996/predict"             #   Localhost endpoint
+model_api = "https://fake-ph-ml.cyclic.app/predict"      #   Cyclic enpoint
 
 # Allow all origins to make CORS request
 origins = [
