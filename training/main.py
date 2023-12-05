2d330c876b6a0c32e7ec898a0c2db36dae88209f
SkyBluePenguin53 <rgnaragdao@gmail.com>
2023-12-05 21:26:07 +0800

    Added grid search tuning
    + Range of parameters of each ML model
    + Grid Search implemented

--- training/main.py
+++ training/main.py
@@ -8,10 +8,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import VotingClassifier
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score, classification_report
 
 from tokenizers import Tokenizer
-from filipino_transformers import TRADExtractor, SYLLExtractor, TextPreprocessor
+from filipino_transformers import TRADExtractor, SYLLExtractor
 
 from json import load as js_load
 
@@ -59,29 +62,75 @@ def custom_tokenizer(doc):
 
 #   Classifiers to test
 classifiers = [
-    ('Multinomial Naive Bayes', MultinomialNB()),
-    ('Logistic Regression', LogisticRegression(random_state=42, max_iter=10000)),
-    ('Random Forest', RandomForestClassifier(random_state=42))
+    {
+        'name': 'Multinomial Naive Bayes',
+        'model': MultinomialNB(),
+        'params': {
+            'classifier__alpha': [0.1, 1.0, 10.0]
+        }
+    },
+    {
+        'name': 'Logistic Regression',
+        'model': LogisticRegression(max_iter=10000, n_jobs=-1),
+        'params': {
+            'classifier__C': [0.1, 1.0, 10.0],
+            'classifier__penalty': ['l1', 'l2']
+        }
+    },
+    {
+        'name': 'Random Forest',
+        'model': RandomForestClassifier(n_jobs=-1),
+        'params': {
+            'classifier__n_estimators': [50, 100, 200],
+            'classifier__max_depth': [None, 10, 20],
+            'classifier__min_samples_split': [2, 5, 10]
+        }
+    },
+    {
+        'name': 'SVC',
+        'model': SVC(),
+        'params': {
+            'classifier__C': [0.1, 1.0, 10.0],
+            'classifier__kernel': ['linear', 'rbf']
+        },
+        'n_jobs': -1 
+    },
+    {
+        'name': 'Voting Classifier',
+        'model': VotingClassifier(estimators=[
+            ('nb', MultinomialNB()),
+            ('lr', LogisticRegression(max_iter=10000, n_jobs=-1)),
+            ('rf', RandomForestClassifier(n_jobs=-1)),
+            ('svc', SVC())
+        ], voting='hard'),
+        'params': {
+            'voting': ['hard', 'soft']
+        }
+    }
 ]
 
+
 #   Test classifiers with pipeline
-for clf_name, clf in classifiers:
-    print(f"\nTesting Model: {clf_name}")
+for clf_info in classifiers:
+    print(f"Training {clf_info['name']}")
+    
+    # Create the pipeline with TruncatedSVD and the specified classifier
     pipeline = Pipeline([
         ('features', FeatureUnion([
-            ('tfidf', TfidfVectorizer(ngram_range=(1, 3), tokenizer=custom_tokenizer)),     #   Get unigrams, bigrams, and trigrams
-            ('bow', CountVectorizer()),                                                     #   Get bag of words
-            ('trad', TRADExtractor()),                                                      #   Extract TRAD features
-            ('syll', SYLLExtractor())                                                       #   Extract SYLL features
+            ('tfidf', TfidfVectorizer(ngram_range=(1, 3), tokenizer=custom_tokenizer)),
+            ('bow', CountVectorizer()),
+            ('trad', TRADExtractor()),
+            ('syll', SYLLExtractor())
         ])),
-        ('classifier', clf)
+        ('classifier', clf_info['model'])
     ])
-
-    #   Fit the entire pipeline on the training data
-    pipeline.fit(X_train, y_train)
+    
+    # Perform grid search
+    grid_search = GridSearchCV(pipeline, clf_info['params'], cv=5, scoring='accuracy')
+    grid_search.fit(X_train, y_train)
 
     #   Make predictions
-    y_pred = pipeline.predict(X_test)
+    y_pred = grid_search.predict(X_test)
 
     #   Evaluate the model
     accuracy = accuracy_score(y_test, y_pred)
