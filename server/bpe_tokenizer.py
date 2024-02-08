872c80a08a33c05f075eef262f88fd498313e348
RB <rbjocsing@up.edu.ph>
2024-02-08 19:48:19 +0800

    Changed /server directories to mirror /training

--- server/bpe_tokenizer.py
+++
@@ -1,8 +0,0 @@
-from tokenizers import Tokenizer
-
-#   Wrapper around pretrained BPE Tokenizer from Cruz et al.
-class BPETokenizer():
-    def __init__(self) -> None:
-        self.tokenizer = Tokenizer.from_file("root/tokenizers/tokenizer.json")
-    def tokenize(self, doc):
-        return self.tokenizer.encode(doc).tokens
