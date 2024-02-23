c89c1c323d61613d376e432e436ead4d6bd87f20
cmpdleon <cmpdleon@gmail.com>
2024-02-24 00:40:03 +0800

    Fixed NaN bug in csv
    Deleted white spaces in column names causing NaN to be written.
    Added to_csv block after for loop.

--- training/pre_extract_morph.py
+++ training/pre_extract_morph.py
@@ -4,15 +4,13 @@ import root.scripts.MORPH as MORPH
 
 def main():
     data = pd.read_csv("root/datasets/FakeNewsFilipino.csv")
-
-
     try:
         morphFeatures = pd.read_csv("root/datasets/MorphFeatures.csv")
     except FileNotFoundError:
-        columns = ("prefix_token_ratio, prefix_derived_ratio, suffix_token_ratio,suffix_derived_ratio, total_affix_token_ratio,"
-                   "total_affix_derived_ratio, actor_focus_ratio, object_focus_ratio, benefactive_focus_ratio, locative_focus_ratio,"
-                   "instrumental_focus_ratio, referential_focus_ratio, infinitive_verb_ratio, participle_verb_ratio,"
-                   "perfective_verb_ratio, imperfective_verb_ratio, contemplative_verb_ratio, recent_past_verb_ratio, aux_verb_ratio")
+        columns = ("prefix_token_ratio,prefix_derived_ratio,suffix_token_ratio,suffix_derived_ratio,total_affix_token_ratio,"
+                   "total_affix_derived_ratio,actor_focus_ratio,object_focus_ratio,benefactive_focus_ratio,locative_focus_ratio,"
+                   "instrumental_focus_ratio,referential_focus_ratio,infinitive_verb_ratio,participle_verb_ratio,"
+                   "perfective_verb_ratio,imperfective_verb_ratio,contemplative_verb_ratio,recent_past_verb_ratio,aux_verb_ratio")
         with open("root/datasets/MorphFeatures.csv", 'w') as f:
             f.write(columns)
         morphFeatures = pd.read_csv("root/datasets/MorphFeatures.csv")
@@ -62,6 +60,10 @@ def main():
             morphFeatures = pd.concat([morphFeatures, pd.DataFrame(list_of_vals, index=list(range(len(list_of_vals))))])
             morphFeatures.to_csv("root/datasets/MorphFeatures.csv", index=False)
             list_of_vals = []
+
+    # Write remaining values to csv
+    morphFeatures = pd.concat([morphFeatures, pd.DataFrame(list_of_vals, index=list(range(len(list_of_vals))))])
+    morphFeatures.to_csv("root/datasets/MorphFeatures.csv", index=False)
 
 if __name__ == "__main__":
     main()
