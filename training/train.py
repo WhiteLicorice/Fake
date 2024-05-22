a4cd2d513117ee357f660ea99ece55b105ef1b01
cmpdleon <cmpdleon@gmail.com>
2024-05-22 09:57:43 +0800

    added print for lr coef

--- training/train.py
+++ training/train.py
@@ -22,6 +22,7 @@ import seaborn as sns
 from datetime import datetime
 
 LOAD_FROM_CSV = True
+FEATURE_COUNT = 47-28
 
 
 session_timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")    # Grab current time to use as timestamp on files
@@ -56,8 +57,10 @@ read_features = pd.concat([read_features_cruz, read_features_lupac], ignore_inde
 lex_features = pd.concat([lex_features_cruz, lex_features_lupac], ignore_index=True)
 morph_features = pd.concat([morph_features_cruz, morph_features_lupac], ignore_index=True)
 
-data = pd.concat([data, trad_features, syll_features, oov_features, sw_features, read_features, lex_features, morph_features], axis=1)
-print(f"HyperParam Testing Results ({session_timestamp})...")
+data = pd.concat([data,trad_features, syll_features, oov_features, sw_features, read_features], axis=1)
+# data.to_csv("root/datasets/All_doc.csv", index=False)
+# exit(0)
+print(f"LR Coefficients ({session_timestamp})...")
 
 # Split the data into features (X) and labels (y)
 y = data['label']  # Labels are 0 -> Fake or 1 -> Real
@@ -69,14 +72,14 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 #   Classifiers to test
 classifiers = [
-    {
-        'name': 'Multinomial Naive Bayes',
-        'model_id': 'MultinomialNB',
-        'model': MultinomialNB(),
-        'params': {
-            'classifier__alpha': [0.1, 1.0, 10.0]
-        }
-    },
+    # {
+    #     'name': 'Multinomial Naive Bayes',
+    #     'model_id': 'MultinomialNB',
+    #     'model': MultinomialNB(),
+    #     'params': {
+    #         'classifier__alpha': [0.1, 1.0, 10.0]
+    #     }
+    # },
     {
         'name': 'Logistic Regression',
         'model_id': 'LogisticRegression',
@@ -85,26 +88,26 @@ classifiers = [
             'classifier__C': [0.1, 1.0, 10.0]
         }
     },
-    {
-        'name': 'Random Forest',
-        'model_id': 'RandomForest',
-        'model': RandomForestClassifier(n_jobs=-1),
-        'params': {
-            'classifier__n_estimators': [50, 100],
-            'classifier__max_depth': [10, 20],
-            'classifier__min_samples_split': [2, 5, 10]
-        }
-    },
-    {
-        'name': 'SVC',
-        'model_id': 'SVC',
-        'model': SVC(),
-        'params': {
-            'classifier__C': [0.1, 1.0, 10.0],
-            'classifier__kernel': ['linear', 'rbf']
-        },
-        'n_jobs': -1 
-    },
+    # {
+    #     'name': 'Random Forest',
+    #     'model_id': 'RandomForest',
+    #     'model': RandomForestClassifier(n_jobs=-1),
+    #     'params': {
+    #         'classifier__n_estimators': [50, 100],
+    #         'classifier__max_depth': [10, 20],
+    #         'classifier__min_samples_split': [2, 5, 10]
+    #     }
+    # },
+    # {
+    #     'name': 'SVC',
+    #     'model_id': 'SVC',
+    #     'model': SVC(),
+    #     'params': {
+    #         'classifier__C': [0.1, 1.0, 10.0],
+    #         'classifier__kernel': ['linear', 'rbf']
+    #     },
+    #     'n_jobs': -1 
+    # },
 ]
 
 # #   Classifiers to test
@@ -133,8 +136,8 @@ for clf_info in classifiers:
         ('sw', StopWordsExtractor(from_csv=LOAD_FROM_CSV)),
         ('trad', TRADExtractor(from_csv=LOAD_FROM_CSV)),                                                                #   Extract TRAD features
         ('syll', SYLLExtractor(from_csv=LOAD_FROM_CSV)),                                                                #   Extract SYLL features
-        ('lex', LEXExtractor(from_csv=LOAD_FROM_CSV)),
-        ('morph', MORPHExtractor(from_csv=LOAD_FROM_CSV))
+        # ('lex', LEXExtractor(from_csv=LOAD_FROM_CSV)),
+        # ('morph', MORPHExtractor(from_csv=LOAD_FROM_CSV))
     ]
     classifier = clf_info['model']
     pipeline = Pipeline(steps=[
@@ -144,34 +147,84 @@ for clf_info in classifiers:
 
     print(f"\nTraining Model: {clf_info['name']}")
 
-    #   Fit the entire pipeline on the training data
+    # #   Fit the entire pipeline on the training data
     pipeline.fit(X_train, y_train)
+
+
+    # GET FEATURE IMPORTANCE (COEFFICIENTS FOR LR)
+    # ----------------------------------------------------------------------------------------
+
+    feature_names = pipeline['features'].get_feature_names_out()
+    feature_names_linguistic = feature_names[-FEATURE_COUNT:]
+    feature_names_vectorizers = feature_names[:-FEATURE_COUNT]
+    assert len(feature_names_linguistic) + len(feature_names_vectorizers) == len(feature_names), "vec and linguistic should be equal to total"
+
+    feature_values = pipeline['classifier'].coef_[0]
+    feature_values_linguistic = feature_values[-FEATURE_COUNT:]
+    feature_values_vectorizers = feature_names[:-FEATURE_COUNT]
+
+    coef_full = dict()
+    coef_linguistic = dict()
+    coef_vect = dict()
+    for i in range(len(feature_names)):
+        coef_full[feature_names[i]] = feature_values[i]
+        if(i > (len(feature_names)) - (FEATURE_COUNT + 1)):
+            coef_linguistic[feature_names[i]] = feature_values[i]
+        else:
+            coef_vect[feature_names[i]] = feature_values[i]
+
+    coef_full = {k: v for k,v in sorted(coef_full.items(), key=lambda item: item[1])}
+    coef_linguistic = {k: v for k,v in sorted(coef_linguistic.items(), key=lambda item: item[1])}
+    coef_vect = {k: v for k,v in sorted(coef_vect.items(), key=lambda item: item[1])}
+
+    print(len(coef_linguistic))
+    print("")
     
-    #   Dump trained model
+
+    print("TOP 10 ON ALL FEATURES")
+    print("------------------------------------")
+    for i, k in enumerate(coef_full):
+        if((i < 9) or (i > len(coef_full)-10)):
+            print(f"{k}: {coef_full[k]}")
+    
+    print("\nTOP 10 ON VECTORIZERS")
+    print("------------------------------------")
+    for i, k in enumerate(coef_vect):
+        if((i < 9) or (i > len(coef_vect)-10)):
+            print(f"{k}: {coef_vect[k]}")
+
+    print("\nALL LINGUISTIC FEATURES")
+    print("------------------------------------")
+    for i, k in enumerate(coef_linguistic):
+        print(f"{k}: {coef_linguistic[k]}")
+    
+    # -------------------------------------------------------------------------------------------
+    
+    # Dump trained model
     # trained_model = pipeline.named_steps['classifier']
     # with open(f"{clf_info['model_id']}_{session_timestamp}.pkl", 'wb') as file:
     #     pickle.dump(pipeline, file)
 
-    #   Make predictions
-    y_pred = pipeline.predict(X_test)
+    # #   Make predictions
+    # y_pred = pipeline.predict(X_test)
 
-    #   Evaluate the model
-    accuracy = accuracy_score(y_test, y_pred)
-    print(f"Accuracy: {accuracy}")
+    # #   Evaluate the model
+    # accuracy = accuracy_score(y_test, y_pred)
+    # print(f"Accuracy: {accuracy}")
 
-    #   Classification report
-    class_report = classification_report(y_test, y_pred)
-    print("Classification Report:\n", class_report)
+    # #   Classification report
+    # class_report = classification_report(y_test, y_pred)
+    # print("Classification Report:\n", class_report)
     
-    #   Confusion Matrix
-    cm = confusion_matrix(y_test, y_pred)
-    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
-    plt.xlabel('Predicted')
-    plt.ylabel('Actual')
-    plt.title(f'Confusion Matrix - {clf_info["name"]}')
-    #plt.show()
-    plt.savefig(f".\\results\\hyperparam\\{clf_info['name']}_none_{session_timestamp}.png", bbox_inches = 'tight')   #   Silently save confusion matrices for overnight training
-    plt.close()
+    # #   Confusion Matrix
+    # cm = confusion_matrix(y_test, y_pred)
+    # sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+    # plt.xlabel('Predicted')
+    # plt.ylabel('Actual')
+    # plt.title(f'Confusion Matrix - {clf_info["name"]}')
+    # #plt.show()
+    # plt.savefig(f".\\results\\hyperparam\\{clf_info['name']}_none_{session_timestamp}.png", bbox_inches = 'tight')   #   Silently save confusion matrices for overnight training
+    # plt.close()
     
 # print("CLASSIFIERS WITH GRIDSEARCH")
 # #   Test classifiers with gridsearch
